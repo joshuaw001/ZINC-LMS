cde04b95daa7de954623ab5bb99e57b7e18bb3f1
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-02-06 18:13:30 -0600

    Update classes.php

--- lms/classes.php
+++ lms/classes.php
@@ -1,1 +1,19 @@
-
+include("db.php");
+$conn = new mysqli($host, $username, $password, "");
+class User {
+  public    $name;
+  public    $username;
+  public    $school_code; 
+  protected $passcode;
+  public    $grade_level;
+  protected $zinc_id;
+  public    $class_codes = Array();
+  
+  function __construct($_name, $_grade, $_class_codes, $_school_code) {
+    $this->name        = $_name;
+    $this->school_code = $_school_code;
+    $this->class_codes = $_class_code;
+    $this->grade_level = $_grade;
+    
+  }
+}
