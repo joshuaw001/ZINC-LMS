2c6e9804a3c102487a058915c2d281554e4c6397
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-02-10 08:42:16 -0600

    Update tags.js

--- lms/TEMPLATE_API/tags.js
+++ lms/TEMPLATE_API/tags.js
@@ -1,1 +1,21 @@
-function CTAG(
+function CTag(tagName,fn){
+  document.createElement(tagName);
+  //find all the tags occurrences (instances) in the document
+  var tagInstances = document.getElementsByTagName(tagName);
+        //for each occurrence run the associated function
+        for ( var i = 0; i < tagInstances.length; i++) {
+            fn(tagInstances[i]);
+        }
+}
+
+function zinc_Question(e) {
+    attr = e.attributes    
+    if(attr.QuestionID && attr.QuestionTitle && attr.Feedback) {
+        ID      = attr.QuestionID.value
+        TITLE   = attr.QuestionTitle.value
+        ANSWERS = querySelectorAll("zinc-answer")
+        Ans     = {};
+        for(j = 0;j < ANSWERS.length; j += 1) {
+            Ans[Answers[j].attributes.answerText.value] = parseFloat(ANSWERS[j].textContent)
+            ANSWER[j].removeChild()
+        e.innerHTML = "<div id='QuestionID-" + ID + "'>"
